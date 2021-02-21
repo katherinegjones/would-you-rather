a1a7d06bdc9f090928d9c1b689bd3644e3eb93d1
Katherine Jones <jones.kjg@gmail.com>
2021-02-20 21:01:34 -0500

    add actions and handlers for answering/adding questions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,8 @@
+import { saveQuestion, saveQuestionAnswer } from '../utils/api'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_QUESTION = 'ADD_QUESTION'
+export const ANSWER_QUESTION = 'ANSWER_QUESTION'
 
 export function receiveQuestions (questions) {
     return {
@@ -6,3 +10,28 @@ export function receiveQuestions (questions) {
         questions,
     }
 }
+
+function addQuestion(question) {
+    return {
+        type: ADD_QUESTION,
+        question
+    }
+}
+
+export function handleAddQuestion (question) {
+    return saveQuestion(question)
+    .then((question) => addQuestion(question))
+}
+
+function answerQuestion(info){
+    return {
+        type: ANSWER_QUESTION,
+        info
+    }
+}
+
+export function handleAnswerQuestion(info) {
+    return saveQuestionAnswer(info)
+    .then((result) => answerQuestion(result))
+}
+
