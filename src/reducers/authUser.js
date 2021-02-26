01916dba3b8dcb65d2cf447a1fd42b4544073cdf
Katherine Jones <jones.kjg@gmail.com>
2021-02-25 20:12:48 -0500

    add reducer for authed user

---
+++ src/reducers/authUser.js
@@ -0,0 +1,11 @@
+import { SET_AUTHED_USER } from '../actions/authUser'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function(state = {}, action){
+    switch(action.type){
+        case SET_AUTHED_USER:
+            return action.id
+        default:
+            return state
+    }
+}
