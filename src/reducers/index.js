841d626871d291bd941c066d5c06858236281506
Katherine Jones <jones.kjg@gmail.com>
2021-02-25 20:18:57 -0500

    add login reducer; combine reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux'
+import questions from './questions'
+import users from './users'
+import authedUser from './authUser'
+import { loadingBarReducer } from 'react-redux-loading'
+
+export default combineReducers ({
+    questions,
+    users,
+    authedUser,
+    loadingBar: loadingBarReducer
+})
