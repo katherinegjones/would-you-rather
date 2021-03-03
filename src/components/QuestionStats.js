53df3f54321184ddde66ffdc388a343ff4e9bfe8
Katherine Jones <jones.kjg@gmail.com>
2021-03-02 20:42:20 -0500

    refactor question stats component to take more props

--- src/components/QuestionStats.js
+++ src/components/QuestionStats.js
@@ -5,25 +5,31 @@ import { PieChart } from 'react-minimal-pie-chart'
 
 class QuestionStats extends Component {
     render() {
-        const { optionOne, optionTwo, selected, num } = this.props
+        const { optionOne, optionTwo, numOneVotes, numTwoVotes, selected, num } = this.props
         return(
            <div className='question-stats-container'>
-               <h2>Question {num}:</h2>
-               {[optionOne, optionTwo].map((item) =>
-                    <div>
-                        {item.name === selected && (
-                            <ImCheckMark className='checkmark'/>
-                        )}
-                        <h3>"{item.name}: {item.numVotes}"</h3>
+               <h2>Question {num}: </h2>
+                    <div className='option-one-stats-container'>
+                            {optionOne === selected && (
+                                <ImCheckMark className='checkmark'/>
+                            )}
+                        <h3 className='option-one-stats'>{optionOne}: {numOneVotes}"</h3>
                     </div>
-               )}
+
+                    <div className='option-two-stats-container'>
+                            {optionTwo === selected && (
+                                <ImCheckMark className='checkmark'/>
+                            )}
+                        <h3 className='option-two-stats'>{optionTwo}: {numTwoVotes}"</h3>
+                    </div>
+                
                 <PieChart 
                     data = {[
-                        {title: `${optionOne.name}`, value: optionOne.numVotes, color: '#009999'},
-                        {title: `${optionTwo.name}`, value: optionTwo.numVotes, color: '#cc0066'}
+                        {title: `${optionOne}`, value: numOneVotes, color: '#009999'},
+                        {title: `${optionTwo}`, value: numTwoVotes, color: '#cc0066'}
                     ]}
                     style={{ height: '100px' }}
-                    label = {({ dataEntry }) => dataEntry.value}
+                    label = {({ dataEntry }) => Math.round(dataEntry.percentage) + '%'}
                     labelStyle = {{
                         fontSize: '5px',
                         fontFamily: 'sans-serif'
